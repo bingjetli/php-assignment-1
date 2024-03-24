4eb3b5d357e3643b366cb7709d921d4e66fedc96
Arnulfo Sanchez <arnulfo457@gmail.com>
2024-03-24 05:10:03 -0400

    added the update and delete cocktail

--- .dbenv.php
+++
@@ -1,7 +0,0 @@
-<?php
-// .dbenv.php
-define('DB_HOST', 'sql311.infinityfree.com');
-define('DB_USER', 'if0_35758270');
-define('DB_PASS', 'rTtUvORAMwAn');
-define('DB_NAME', 'if0_35758270_bar_pos_input_db');
-?>
